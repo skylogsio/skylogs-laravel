53e72176141e7d6109e94ac9d5c361d468373127
moheb79 <mhmdhbhadr@gmail.com>
2025-02-22 20:40:27 +0330

    chore: create a basic type for CreateOrUpdate modals

--- apps/frontend/src/@types/global.ts
+++ apps/frontend/src/@types/global.ts
@@ -1,1 +1,8 @@
+import type{ ModalContainerProps } from "@/components/Modal/types";
+
 export type CreateUpdateModal<T> = T | "NEW" | null;
+
+export interface BasicCreateOrUpdateModalProps
+  extends Pick<ModalContainerProps, "open" | "onClose"> {
+  onSubmit: () => void;
+}
