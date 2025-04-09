a48f6d0312ac9d12d018a66c7536bd656a10cf95
moheb79 <mhmdhbhadr@gmail.com>
2025-04-09 21:29:26 +0330

    fix: make data-source requests to be sent from the NextJs server

--- apps/frontend/src/api/dataSource.ts
+++ apps/frontend/src/api/dataSource.ts
@@ -1,15 +1,24 @@
+"use server";
+
+import type { ServerResponse } from "@/@types/global";
 import axios from "@/lib/axios";
 
 const DATA_SOURCE_URL = "data-source";
 
-export function createDataSource(body: unknown) {
-  return axios.post(DATA_SOURCE_URL, body);
+export async function createDataSource(body: unknown) {
+  return axios
+    .post<ServerResponse<unknown>>(DATA_SOURCE_URL, body)
+    .then((response) => response.data);
 }
 
-export function updateDataSource(dataSourceId:unknown,body: unknown) {
-  return axios.put(`${DATA_SOURCE_URL}/${dataSourceId}`, body);
+export async function updateDataSource(dataSourceId: unknown, body: unknown) {
+  return axios
+    .put<ServerResponse<unknown>>(`${DATA_SOURCE_URL}/${dataSourceId}`, body)
+    .then((response) => response.data);
 }
 
-export function deleteDataSource(dataSourceId:unknown) {
-  return axios.delete(`${DATA_SOURCE_URL}/${dataSourceId}`);
+export async function deleteDataSource(dataSourceId: unknown) {
+  return axios
+    .delete<ServerResponse<unknown>>(`${DATA_SOURCE_URL}/${dataSourceId}`)
+    .then((response) => response.data);
 }
