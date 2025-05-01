50d3d75a4ae905a2d27ef60fe58e7692193d6b92
moheb79 <mhmdhbhadr@gmail.com>
2025-05-01 21:47:11 +0330

    refact: 🎨Do error handling for apis

--- apps/frontend/src/api/endpoint.ts
+++ apps/frontend/src/api/endpoint.ts
@@ -5,18 +5,35 @@ import axios from "@/lib/axios";
 
 const ENDPOINT_URL = "endpoint";
 
-export async function createEndpoint(body: unknown) {
-  return axios.post<ServerResponse<unknown>>(ENDPOINT_URL, body).then((response) => response.data);
+export async function createEndpoint(body: unknown): Promise<ServerResponse<unknown>> {
+  try {
+    const response = await axios.post<ServerResponse<unknown>>(ENDPOINT_URL, body);
+    return response.data;
+  } catch (error) {
+    throw error;
+  }
 }
 
-export async function updateEndpoint(endpointId: unknown, body: unknown) {
-  return axios
-    .put<ServerResponse<unknown>>(`${ENDPOINT_URL}/${endpointId}`, body)
-    .then((response) => response.data);
+export async function updateEndpoint(
+  endpointId: string,
+  body: unknown
+): Promise<ServerResponse<unknown>> {
+  try {
+    const response = await axios.put<ServerResponse<unknown>>(
+      `${ENDPOINT_URL}/${endpointId}`,
+      body
+    );
+    return response.data;
+  } catch (error) {
+    throw error;
+  }
 }
 
-export async function deleteEndpoint(endpointId: unknown) {
-  return axios
-    .delete<ServerResponse<unknown>>(`${ENDPOINT_URL}/${endpointId}`)
-    .then((response) => response.data);
+export async function deleteEndpoint(endpointId: string): Promise<ServerResponse<unknown>> {
+  try {
+    const response = await axios.delete<ServerResponse<unknown>>(`${ENDPOINT_URL}/${endpointId}`);
+    return response.data;
+  } catch (error) {
+    throw error;
+  }
 }
--- apps/frontend/src/api/user.ts
+++ apps/frontend/src/api/user.ts
@@ -1,29 +1,46 @@
 "use server";
 
-import type{ ServerResponse } from "@/@types/global";
+import type { ServerResponse } from "@/@types/global";
 import axios from "@/lib/axios";
 
 const USER_URL = "user";
 const USER_PASSWORD_URL = "user/pass";
 
 export async function createUser(body: unknown) {
-  return axios.post<ServerResponse<unknown>>(USER_URL, body).then((response) => response.data);
+  try {
+    const response = await axios.post<ServerResponse<unknown>>(USER_URL, body);
+    return response.data;
+  } catch (error) {
+    throw error;
+  }
 }
 
-export async function updateUser(userId: unknown, body: unknown) {
-  return axios
-    .put<ServerResponse<unknown>>(`${USER_URL}/${userId}`, body)
-    .then((response) => response.data);
+export async function updateUser(userId: string, body: unknown) {
+  try {
+    const response = await axios.put<ServerResponse<unknown>>(`${USER_URL}/${userId}`, body);
+    return response.data;
+  } catch (error) {
+    throw error;
+  }
 }
 
-export async function deleteUser(userId: unknown) {
-  return axios
-    .delete<ServerResponse<unknown>>(`${USER_URL}/${userId}`)
-    .then((response) => response.data);
+export async function deleteUser(userId: string) {
+  try {
+    const response = await axios.delete<ServerResponse<unknown>>(`${USER_URL}/${userId}`);
+    return response.data;
+  } catch (error) {
+    throw error;
+  }
 }
 
-export async function changePassword(userId: unknown, body: unknown) {
-  return axios
-    .put<ServerResponse<unknown>>(`${USER_PASSWORD_URL}/${userId}`, body)
-    .then((response) => response.data);
+export async function changePassword(userId: string, body: unknown) {
+  try {
+    const response = await axios.put<ServerResponse<unknown>>(
+      `${USER_PASSWORD_URL}/${userId}`,
+      body
+    );
+    return response.data;
+  } catch (error) {
+    throw error;
+  }
 }
