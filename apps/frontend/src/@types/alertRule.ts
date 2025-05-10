eef433a3ef56dca61fd5b0e07e6b98731fc1b534
mhmdhbhadr <mhmdhbhadr@gmail.com>
2025-05-10 17:24:09 +0330

    chore: 👽️ Remove additional fields from create-data api

--- apps/frontend/src/@types/alertRule.ts
+++ apps/frontend/src/@types/alertRule.ts
@@ -2,9 +2,6 @@ import type { IEndpoint } from "@/@types/endpoint";
 import type { IUser } from "@/@types/user";
 
 export interface IAlertRuleCreateData {
-  prometheusDataSources: string[];
-  grafanaDataSources: string[];
-  splunkDataSources: string[];
   endpoints: IEndpoint[];
   users: IUser[];
 }
