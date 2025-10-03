3c8bfbddd2dbabe083b803079c0fc7f5a32ab18f
moheb79 <mhmdhbhadr@gmail.com>
2025-10-03 21:53:05 +0330

    fix: Fix updating endpoint flow

--- apps/frontend/src/api/flow.ts
+++ apps/frontend/src/api/flow.ts
@@ -6,25 +6,6 @@ import axios from "@/lib/axios";
 
 const FLOW_URL = "endpoint";
 
-export async function createFlow(body: unknown): Promise<ServerResponse<unknown>> {
-  try {
-    const response = await axios.post<ServerResponse<unknown>>(FLOW_URL, body);
-    return response.data;
-  } catch (error) {
-    console.log("🚀 ~ createFlow ~ error:", error);
-    throw error;
-  }
-}
-
-export async function updateFlow(flowId: string, body: unknown): Promise<ServerResponse<unknown>> {
-  try {
-    const response = await axios.put<ServerResponse<unknown>>(`${FLOW_URL}/${flowId}`, body);
-    return response.data;
-  } catch (error) {
-    throw error;
-  }
-}
-
 export async function deleteFlow(flowId: string): Promise<ServerResponse<unknown>> {
   try {
     const response = await axios.delete<ServerResponse<unknown>>(`${FLOW_URL}/${flowId}`);
